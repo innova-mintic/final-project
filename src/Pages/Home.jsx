d33d9ae5c64e4717d6d9fc87aee394aa449f2bf1
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-12-01 11:15:25 -0500

    dejando el home listo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'components/NavBar';
 import 'styles/home.css';
+import Footer from 'components/Footer';
 
  const Home = () => {
 
@@ -8,8 +9,12 @@ import 'styles/home.css';
         <div className="background">
             <Navbar />
             <div className="intro">
-                inscribe tus proyectos
+                <div className="description">
+                    Disfruta este gestor de proyectos, registra tu equipo y comienza a administrarlo.
+                    <h3 className="h3main">¡Que esperas!</h3>
+                </div>
             </div>
+            <Footer />
         </div>
     )
 }
